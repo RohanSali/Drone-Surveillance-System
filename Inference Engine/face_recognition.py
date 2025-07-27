d03f5ac576d007e8a8c158ddb2c9d9324f1f8890
RohanSali <rohansali31@gmail.com>
2025-07-27 15:26:09 +0530

    new classifier - face recognition for lost finding added!

---
+++ Inference Engine/face_recognition.py
@@ -0,0 +1,100 @@
+import torch
+from facenet_pytorch import InceptionResnetV1, MTCNN
+from PIL import Image
+import numpy as np
+import cv2
+import ast
+import base64
+import requests
+from collections import deque
+from datetime import datetime
+
+URL = ""
+TEMP_TEXT_FILE = 'Inference Engine/person_found.txt'
+TIME_THRESHOLD = 60 #sec
+
+# Load pretrained FaceNet model
+model = InceptionResnetV1(pretrained='vggface2').eval()
+mtcnn = MTCNN(image_size=160, margin=0)
+
+def get_embedding_from_frame(frame):
+    # Convert to RGB PIL Image
+    img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    face = mtcnn(img)
+    if face is not None:
+        return model(face.unsqueeze(0))
+    else:
+        return None
+
+def encode_frame(frame):
+    # Encode the frame to JPEG format
+    success, encoded_image = cv2.imencode('.jpg', frame)
+    if not success:
+        raise ValueError("Image encoding failed")
+
+    # Convert to base64
+    base64_image = base64.b64encode(encoded_image).decode('utf-8')
+    return base64_image
+
+def send_to_server(payload):
+    response = requests.post(URL,json=payload)
+    # json.dumps(payload)
+    if response.ok :
+        print(f"✅ Sent to server as {payload['alert']}")
+
+def save_to_machine(payload):
+    with open(TEMP_TEXT_FILE, 'a') as f:
+        f.write(str(payload) + '\n')
+    print(f"✅ Saved to machine as {payload['Timestamp']}")
+
+
+def compare_faces(frame1, frame2 , name ,capture_timestamp):
+    emb1 = get_embedding_from_frame(frame1)
+    emb2 = get_embedding_from_frame(frame2)
+
+    if emb1 is not None and emb2 is not None:
+        cosine_sim = torch.nn.functional.cosine_similarity(emb1, emb2).item()
+        threshold = 0.6
+        if cosine_sim > threshold:
+            print("Faces match! Similarity:", cosine_sim , "Person found at timestamp : ",capture_timestamp)
+
+            frame1_blob = encode_frame(frame1)
+            frame2_blob = encode_frame(frame2)
+            payload = {
+                "Found":1,
+                "Name":name,
+                "Actual Image":frame1_blob,
+                "Matched Frame" : frame2_blob,
+                "Location" : (0,0,0),
+                "Timestamp":capture_timestamp.isoformat()
+            }
+
+            timestamp = None
+            with open(TEMP_TEXT_FILE, 'r') as file:
+                lines = deque(file, maxlen=None)
+
+            for line in reversed(lines):
+                try:
+                    data = ast.literal_eval(line.strip())
+                    if isinstance(data, dict) and data.get('Name') == name:
+                        timestamp = datetime.fromisoformat(data.get('Timestamp'))
+                        break
+                except Exception as e:
+                    print(f"❌ Error parsing line: {line} -> {e}")
+
+            if timestamp :
+                time_difference = (capture_timestamp - timestamp).total_seconds()
+                print(f"Time difference between last found for  is : {time_difference} ⏳")
+                if time_difference > TIME_THRESHOLD:
+                    save_to_machine(payload)
+                    #send_to_server(payload)
+            else :
+                print(f"🔒 Saving {name} for first time!")
+                save_to_machine(payload)
+                #send_to_server(payload)
+
+
+        else:
+            print("Faces do not match. Similarity:", cosine_sim)
+    else:
+        print("Face not detected in one of the frames.")
--- Inference Engine/runner.py
+++ Inference Engine/runner.py
@@ -27,16 +27,47 @@
 # cv2.destroyAllWindows()
 
 
+import os
 import cv2
 import threading
 import queue
 from datetime import datetime
 from casulty_inference import inference_casulty
 from anamoly_inference import inference_anamoly
+from face_recognition import compare_faces
+from pathlib import Path
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+def create_required_files_and_folders():
+    lost_person_folder = os.path.join(base_dir, 'lost_person')
+    alerts_file = os.path.join(base_dir,'alerts.txt')
+    person_found_file = os.path.join(base_dir,'person_found.txt')
+
+    # Create the folder if it doesn't exist
+    if not os.path.exists(lost_person_folder):
+        os.makedirs(lost_person_folder)
+        print(f"Created folder: {lost_person_folder}")
+
+    # Create the file if it doesn't exist
+    if not os.path.exists(alerts_file):
+        with open(alerts_file, 'w') as f:
+            f.write('')  # Or write default content
+        print(f"Created file: {alerts_file}")
+    
+    if not os.path.exists(person_found_file):
+        with open(person_found_file, 'w') as f:
+            f.write('')  # Or write default content
+        print(f"Created file: {person_found_file}")
+
+create_required_files_and_folders()
+
+LOST_FINDING_IMG_PATH = Path(os.path.join(base_dir, 'lost_person\image1.jpg'))
 
 # Define 1-slot queues for real-time inference (latest frame only)
 casulty_queue = queue.Queue(maxsize=1)
 anamoly_queue = queue.Queue(maxsize=1)
+match_face_queue = queue.Queue(maxsize=1)
 
 # Function to safely insert latest frame into a queue (replaces old)
 def safe_put(q, item):
@@ -61,9 +92,22 @@ def anamoly_worker():
         inference_anamoly(frame, timestamp)
         anamoly_queue.task_done()
 
+# Async inference thread for anomaly
+def match_face_worker():
+    while True:
+        frame2 = cv2.imread(LOST_FINDING_IMG_PATH)
+        
+        if frame2 is None:
+            return
+        
+        frame, timestamp = match_face_queue.get()
+        compare_faces(frame, frame2, LOST_FINDING_IMG_PATH.stem , timestamp)
+        match_face_queue.task_done()
+
 # Start both threads as daemons
 threading.Thread(target=casulty_worker, daemon=True).start()
 threading.Thread(target=anamoly_worker, daemon=True).start()
+threading.Thread(target=match_face_worker, daemon=True).start()
 
 # Capture from webcam or drone feed
 cap = cv2.VideoCapture(0)
@@ -79,6 +123,7 @@ while True:
     # Send to inference queues (copy frame to avoid race condition)
     safe_put(casulty_queue, (frame.copy(), timestamp))
     safe_put(anamoly_queue, (frame.copy(), timestamp))
+    safe_put(match_face_queue, (frame.copy(), timestamp))
 
     frame_id += 1
 
